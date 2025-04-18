bb85d6fd0108ddd63655f3f20b296478a6658715
Larissa <larissafdds@gmail.com>
2025-04-17 23:22:30 -0300

    Update plotar.m

--- CONTAGEM/plotar.m
+++ CONTAGEM/plotar.m
@@ -1,7 +1,7 @@
-% === Ler o CSV ===
+% Ler o arquivo
 dados = readtable('dados_comparacao.csv');
 
-% === Ordenar pela coluna 'Procurado' ===
+% Ordenar pela coluna 'Procurado'
 dados = sortrows(dados, 'Procurado');
 
 % Dados
@@ -18,25 +18,31 @@ for i = 1:height(amostra)
     subplot(2,1,1);
     scatter(amostra.Procurado(1:i), amostra.Comparacoes_ABB(1:i), 30, 'b', 'filled');
     hold on;
+
+    % Media ABB
     media_abb = mean(amostra.Comparacoes_ABB(1:i));
     yline(media_abb, '--black', sprintf('Média ABB = %.1f', media_abb), ...
           'LabelHorizontalAlignment', 'left', 'LineWidth', 1.5);
     title(sprintf('ABB - Comparações até %d valores', i));
-    ylabel('Comparações (log)');
-    set(gca, 'YScale', 'log');
+    
+    %ylabel('Comparações (log)');
+    %set(gca, 'YScale', 'log');
     grid on;
 
     % ==== SUBPLOT 2: Lista Encadeada ====
     subplot(2,1,2);
-    scatter(amostra.Procurado(1:i), amostra.Comparacoes_Lista(1:i), 30, 'r', 'x');
+    scatter(amostra.Procurado(1:i), amostra.Comparacoes_Lista(1:i), 30, 'r', 'filled');
     hold on;
+    
+    % Media lista
     media_lista = mean(amostra.Comparacoes_Lista(1:i));
     yline(media_lista, '--black', sprintf('Média Lista = %.1f', media_lista), ...
           'LabelHorizontalAlignment', 'left', 'LineWidth', 1.5);
     title(sprintf('Lista Encadeada - Comparações até %d valores', i));
     xlabel('Valor Procurado');
-    ylabel('Comparações (log)');
-    set(gca, 'YScale', 'log');
+    
+    %ylabel('Comparações (log)');
+    %set(gca, 'YScale', 'log');
     grid on;
 
     % Ajustes finais e captura do frame
